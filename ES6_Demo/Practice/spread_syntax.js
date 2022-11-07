247a5a4a35f8ea49d3fb9cd674d4d7f92f56eb3a
DragonRF <duonglong410@gmail.com>
2022-11-07 16:47:40 +0700

    first commit

--- ES6_Demo/Practice/spread_syntax.js
+++ ES6_Demo/Practice/spread_syntax.js
@@ -7,3 +7,26 @@ let testArray = [1, 2, 100, 100, 6]
 let replaceArray = [3, 4, 5]
 replace(testArray, 2, 4, replaceArray)
 console.log(testArray)
+
+
+function copyReplace(array, from, to, elements) {
+
+    return array.slice(0, from).concat(elements).concat(array.slice(to))
+
+}
+let myArray = [1, 2, 100, 200, 6];
+let myRArray = [3, 4, 5];
+
+console.log(copyReplace(myArray, 2, 4, myRArray))
+
+let customerOrder = []
+
+
+function recordOrders(time) {
+
+    customerOrder.push({time, birds: Array.prototype.slice.call(arguments, 1)})
+
+}
+let myFood = ["coffee", "yogurt", "pizza"]
+recordOrders(new Date, ...myFood);
+console.log(customerOrder)
