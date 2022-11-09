285f7b9182cbe8f934cc599057f55fb1e0e6aabe
DragonRF <duonglong410@gmail.com>
2022-11-09 15:00:07 +0700

    first commit

---
+++ access-modifier_static-method/EmployeeManager/EmployeeManager.ts
@@ -0,0 +1,88 @@
+class EmployeeManager {
+    private firstName: string;
+    private lastName: string;
+    private birthDay: string;
+    private address: string;
+    private position: string;
+    static employee: object[] = [];
+
+    constructor(firstName: string, lastName: string, birthDay: string, address: string, position: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.address = address;
+        this.position = position;
+    }
+    static display(){
+        return EmployeeManager.employee;
+    }
+    static add(employee: object){
+        return EmployeeManager.employee.push(employee);
+    }
+    static getInfo(employee: object){
+        return employee;
+    }
+    static delete(employee: object){
+        if(EmployeeManager.employee.indexOf(employee) !== -1){
+            return EmployeeManager.employee.splice(EmployeeManager.employee.indexOf(employee),1)
+        }
+
+    }
+    static edit(employee1: object ,employee2: object ){
+        EmployeeManager.employee[EmployeeManager.employee.indexOf(employee1)] = employee2 ;
+        return EmployeeManager.employee;
+    }
+
+    getFirstName(): string {
+        return this.firstName;
+    }
+
+    setFirstName(value: string) {
+        this.firstName = value;
+    }
+
+    getLastName(): string {
+        return this.lastName;
+    }
+
+    setLastName(value: string) {
+        this.lastName = value;
+    }
+
+    getBirthDay(): string {
+        return this.birthDay;
+    }
+
+    setBirthDay(value: string) {
+        this.birthDay = value;
+    }
+
+    getAddress(): string {
+        return this.address;
+    }
+
+    setAddress(value: string) {
+        this.address = value;
+    }
+
+    getPosition(): string {
+        return this.position;
+    }
+
+    setPosition(value: string) {
+        this.position = value;
+    }
+}
+
+let employee1 = new EmployeeManager('1','2','3','4','5')
+let employee2 = new EmployeeManager('6','7','8','9','10')
+let employee3 = new EmployeeManager('11','12','13','14','15')
+
+EmployeeManager.add(employee1)
+EmployeeManager.add(employee2)
+EmployeeManager.add(employee3)
+
+EmployeeManager.delete(employee1)
+
+EmployeeManager.edit(employee2,employee1)
+console.log(EmployeeManager.display());
